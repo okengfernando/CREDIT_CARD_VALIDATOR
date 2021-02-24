4dc39d9ba17aea35fc5e1428931f6a16512a35aa
fernando <okengfernando@github.com>
2021-02-24 17:34:25 +0300

    fixed credit card tester methods

--- driver_code.py
+++ driver_code.py
@@ -1,7 +1,15 @@
 import Credit_Card_Validator as cr
 
 if __name__ == '__main__':
-    card_number = int(input("Enter Card No :"))
+    card_number = (input("Enter Card No in format (XXXX-XXXX or XXXX XXXX or XXXXXXXX) :"))
+
+    if " " in card_number:
+        card_number = int(card_number.replace(" ",""))
+    elif "-" in card_number:
+        card_number = int(card_number.replace("-",""))
+    
+    
+
     card = cr.CreditCard.set_card(card_number)
     print(card.company)
     print('Card : ', card.card_no)
